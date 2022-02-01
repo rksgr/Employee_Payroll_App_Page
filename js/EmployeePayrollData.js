99f5a54065232f796414b6882cca05ff809ad13a
rksgr <r_kjha@yahoo.com>
2022-02-01 07:32:54 +0530

    Refactored: Employee payroll class with new attributes and their getters and setters

---
+++ js/EmployeePayrollData.js
@@ -0,0 +1,39 @@
+class EmployeePayrollData{
+
+    //getter and setter methods
+    get id(){return this._id;}
+    set id(id){this._id = id;}
+
+    get name(){return this._name;}
+    set name(name){this._name=name;}
+
+    get department(){return this._department;}
+    set department(department){this._department=department;}
+
+    get profilePic(){return this._profilePic;}
+    set profilePic(profilePic){this._profilePic = profilePic;}
+
+    get gender(){return this._gender;}
+    set gender(gender){this._gender = gender;}
+
+    get salary(){return this._salary;}
+    set salary(){this._salary=salary;}
+
+    get note(){return this._note;}
+    set note(note){this._note = note;}
+
+    get startDate(){return this._startDate;}
+    set startDate(startDate){this._startDate = startDate;}
+
+    //method
+    toString(){
+        const options={year:'numeric',month:'long',day:'numeric'};
+        const empDate = !this.startDate ?   "undefined" :
+                        this.startDate.toLocalDateString("en-US",options);
+
+        return "id=" + this.id + ",name='"+this.name+",gender='"+this.gender+
+                ",profilePic='"+this.profilePic+", department="+this.department+
+                ", salary="+this.salary+",startDate="+empDate+",note="+this.note;
+    }
+}
+
